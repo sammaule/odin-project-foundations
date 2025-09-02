1b56ff180a879229c3ada8c40f1562c68d082fd3
Sam Maule <sam.maule@gmail.com>
2025-09-02 15:00:28 +0100

    webpack-practice

---
+++ webpack-practice/src/index.js
@@ -0,0 +1,9 @@
+// index.js
+import "./styles.css";
+import { greeting } from "./greeting.js";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+console.log(greeting);
